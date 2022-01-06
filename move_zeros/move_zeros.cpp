d5dd94422c2a47de3a64e2b2d04553dee876435f
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2022-01-06 19:31:06 +0100

    modified:   CMakeLists.txt 	modified:   move_zeros/move_zeros.cpp

--- move_zeros/move_zeros.cpp
+++ move_zeros/move_zeros.cpp
@@ -6,7 +6,7 @@ using std::vector;
 using std::begin;
 using std::end;
 /**
- *  Problem statement:
+ * Problem statement:
  * Given an integer array nums, move all 0's to the 
  * end of it while maintaining the relative order of the non-zero elements.
  *  Note that you must do this in-place without making a copy of the array.
