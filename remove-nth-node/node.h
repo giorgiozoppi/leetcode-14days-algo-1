377e19b365f7b3892102147d428c6f9c9ccd0ee8
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2022-01-07 19:26:24 +0100

    Remove N nodes 	new file:   node.h 	new file:   remove-nth-node.cpp

---
+++ remove-nth-node/node.h
@@ -0,0 +1,8 @@
+#ifndef NODE_H
+#define NODE_H
+#include <memory>
+struct ListNode {
+    int val;
+    ListNode* next;
+};
+#endif
---
+++ remove-nth-node/remove-nth-node.cpp
@@ -0,0 +1,48 @@
+/**
+ * Problem:
+ * Given the head of a linked list, remove the nth node 
+ * from the end of the list and return its head.
+ * 
+ */
+#include "node.h"
+
+namespace leetcode {
+    int size_list(ListNode* head) {
+      auto size{1};
+      for(auto tmp = head; tmp->next != nullptr; tmp = tmp->next) {
+          size++;
+      }
+      return size;
+    }
+    ListNode* removeNodeEven(ListNode* head, int n) {
+        if (n == 1) {
+            delete head;
+            return nullptr;
+        } else {
+            auto tmp = head->next;
+            delete head;
+            head = tmp;
+            return head;
+        }
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+      if ((head == nullptr) || (n <=0)) {
+          return head;
+      }        
+      int max_loop = size_list(head) - n;
+      ListNode* slow = head;
+      ListNode* fast = head;
+      if (max_loop == 0) {
+          return removeNodeEven(head, max_loop);
+      }
+      while( (fast != nullptr) && (max_loop > 0)) {
+          slow = fast;
+          fast = fast->next;
+          max_loop--;
+      }
+      if (fast != nullptr) {
+        slow->next = fast->next;
+      }
+      return head;
+    }  
+};
